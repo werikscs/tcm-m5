7689bb30b717c2fc67a6364a219ce91385497b91
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-09-08 12:13:48 -0300

    feat: create users model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class User(AbstractUser):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(unique=True, max_length=20)
+    email = models.CharField(unique=True, max_length=127)
+    birthdate = models.DateField()
