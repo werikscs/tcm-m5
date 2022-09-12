087a8fc852b3319e159345fde1b170c72430ccec
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-09-12 14:37:03 -0300

    refactor: change environment variables

--- world_cup_ecommerce/settings.py
+++ world_cup_ecommerce/settings.py
@@ -88,9 +88,9 @@ WSGI_APPLICATION = "world_cup_ecommerce.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
         "HOST": "127.0.0.1",
         "PORT": 5432,
     }
