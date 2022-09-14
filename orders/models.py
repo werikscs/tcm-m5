12d680ac2559ad0756cc4f70df97c9ef19d93d06
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-09-14 16:49:42 -0300

    feat: create order model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Order(models.Model):
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="orders"
+    )
