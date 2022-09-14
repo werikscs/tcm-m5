c3ea14fa23b197276edd4073a2b8b34f8ce7b62b
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-09-14 16:02:49 -0300

    refactor: change url key from int to pk

--- discounts/urls.py
+++ discounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import DiscountView, DiscountDetailView
+from .views import DiscountDetailView, DiscountView
 
 urlpatterns = [
     path("discount/", DiscountView.as_view()),
-    path("discount/<int:discount_id>/", DiscountDetailView.as_view())
+    path("discount/<pk>/", DiscountDetailView.as_view()),
 ]
