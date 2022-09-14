f1dd95a1251a73bf43596ecea1f41495a7740cf6
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-09-14 15:57:18 -0300

    refactor: change field quantity in Cartproducts to quantity_in_cart

--- cartproducts/models.py
+++ cartproducts/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 
 class Cartproducts(models.Model):
-    quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)], default=1)
+    quantity_in_cart = models.PositiveIntegerField(
+        validators=[MinValueValidator(1)], default=1
+    )
 
     cart = models.ForeignKey(
         "carts.Cart", on_delete=models.CASCADE, related_name="cartproducts"
