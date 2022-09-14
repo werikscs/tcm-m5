d31fd812dba9942408920cc40daba49641042024
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-09-14 10:21:11 -0300

    refactor: change allowed hosts list

--- world_cup_ecommerce/settings.py
+++ world_cup_ecommerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["tcm-m5.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["tcm-m5.herokuapp.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
