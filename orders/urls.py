a88a1b5d55db233636ac9a5dbf29992fa001ab85
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-09-14 20:00:23 -0300

    feat: add a url to get orders by especific ordder id

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import OrderView
+from .views import OrderDetailView, OrderView
 
 urlpatterns = [
     # criar ordem de user ou pegar todas as ordens do user
     path("order/<int:user_id>/", OrderView.as_view()),
     # pegar ordem específica de user
-    # path("order/<int:user_id>/<int:order_id>/", ),
+    path("order/<int:user_id>/<int:order_id>/", OrderDetailView.as_view()),
 ]
