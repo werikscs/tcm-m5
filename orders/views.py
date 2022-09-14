736d319602192296165f9e3aa7de3415fd0b9713
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-09-14 18:11:14 -0300

    feat: create view to handle create Orders

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+
+from orders.models import Order
+
+from .serializers import OrderSerializer
+
 
 # Create your views here.
+class OrderView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    authentication_classes = [TokenAuthentication]
+    serializer_class = OrderSerializer
