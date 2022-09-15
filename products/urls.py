288c62f9753ae3d5ee7f29ad61135df3c36c5019
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-09-14 23:25:31 -0300

    feat: create view to show products by category

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ProductDetailView, ProductView
+from .views import ProductCategoryView, ProductDetailView, ProductView
 
 urlpatterns = [
     path("products/", ProductView.as_view()),
-    path("products/<pk>/", ProductDetailView.as_view())
+    path("products/<pk>/", ProductDetailView.as_view()),
+    path("products/category/<int:category_id>/", ProductCategoryView.as_view()),
 ]
